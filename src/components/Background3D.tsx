4935493d3670843a37b59d082c0dd0ec71e8a247
Sahoo999 <sahoodeb04@gmail.comm>
2026-01-18 13:36:10 +0530

    feat: add `Background3D` component for an animated starfield using React Three Fiber.

--- src/components/Background3D.tsx
+++ src/components/Background3D.tsx
@@ -5,12 +5,14 @@ import { Points, PointMaterial, Preload } from '@react-three/drei';
 import * as random from 'maath/random/dist/maath-random.esm';
 
 const StarField = (props: any) => {
-    const ref = useRef<any>();
+    const ref = useRef<any>(null);
     const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
 
-    useFrame((state, delta) => {
-        ref.current.rotation.x -= delta / 10;
-        ref.current.rotation.y -= delta / 15;
+    useFrame((_state, delta) => {
+        if (ref.current) {
+            ref.current.rotation.x -= delta / 10;
+            ref.current.rotation.y -= delta / 15;
+        }
     });
 
     return (
